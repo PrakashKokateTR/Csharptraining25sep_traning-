14a2f71c6f36634a3ec470db997b5752438adf87
6124621 <6124621@TR-1QCTDX3>
2024-09-25 08:48:58 +0530

    Add project files.

---
+++ MedicalFrameAutomationPK/MedicalAutomation.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalFrameAutomationPK
+{
+    public class MedicalAutomation
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void BeforeTestMethod()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.Navigate().GoToUrl("https://demo.openemr.io/b/openemr");
+
+
+        }
+        [TearDown]
+        public void AfterTestMethod()
+        {
+            driver.Dispose();
+        }
+
+        [Test]
+        public void TitleTest()
+        {
+
+
+            string actualTitle = driver.Title;
+            Assert.That(actualTitle, Is.EqualTo("OpenEMR Login"));
+        }
+
+        [Test]
+        public void ApplicationDescriptionTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+
+            driver.Navigate().GoToUrl("https://demo.openemr.io/b/openemr");
+
+            //Assert the text --> "The most popular open-source Electronic Health Record and Medical Practice Management solution."
+            string actualDescription = driver.FindElement(By.XPath("//p[contains(text(),'most')]")).Text;
+            Assert.That(actualDescription, Is.EqualTo("The most popular open-source Electronic Health Record and Medical Practice Management solution."));
+        }
+    }
+}
